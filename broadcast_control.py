438a4c488a4b74874cf2de4c273e398f77dd5801
JYOTHIKA M <mjyothika79@gmail.com>
2026-04-17 12:45:32 +0530

    Add files via upload

---
+++ broadcast_control.py
@@ -0,0 +1,54 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet
+
+class BroadcastControl(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(BroadcastControl, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        dst = eth.dst
+        src = eth.src
+        dpid = datapath.id
+        in_port = msg.match['in_port']
+
+        self.mac_to_port.setdefault(dpid, {})
+
+        # Learn MAC
+        self.mac_to_port[dpid][src] = in_port
+
+        # 🚨 BROADCAST DETECTION
+        if dst == "ff:ff:ff:ff:ff:ff":
+            self.logger.info("Broadcast detected → limiting flooding")
+            return  # BLOCK BROADCAST
+
+        # NORMAL FORWARDING
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=msg.data
+        )
+        datapath.send_msg(out)
